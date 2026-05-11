b091327f3a402ab4a1f1785466130195f0b4210c
Eduardo220898 <eduardozollner@gmail.com>
2026-05-11 20:22:58 -0300

    post

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const app = express()
 
 app.get('/usuarios', (req, res) => {
 
-    res.send('Olá seja bem vindo' )
+    res.send('Olá seja bem vindo')
+})
+
+app.post('/usuarios', (req, res) => {
+
+    res.send('joia')
 })
 
 
