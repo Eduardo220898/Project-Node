a85f6740fe90fa67f84616f228bfb23844ffeef7
Eduardo220898 <eduardozollner@gmail.com>
2026-05-12 15:11:24 -0300

    DataBase

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
 import express from 'express'
+import { PrismaClient} from './generated/Prisma/Client.js'
 
 
 
 
 
 const app = express()
+app.use(express.json())
+
+const users = []
+
+const prisma = new PrismaClient();
+
 
 app.get('/usuarios', (req, res) => {
 
-    res.send('Olá seja bem vindo')
+    res.status(200).json(users)
 })
 
 app.post('/usuarios', (req, res) => {
 
-    res.send('joia')
+    users.push(req.body)
+
+    res.status(201).json({
+        message: 'Usuario criado com sucesso'
+    })
 })
 
 
